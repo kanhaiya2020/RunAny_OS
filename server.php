c1d5b670723be431aff7b7e88434cdcfc5ed0207
kanhaiya2020 <34909088+kanhaiya2020@users.noreply.github.com>
2020-02-18 13:12:18 +0530

    index and server file

---
+++ server.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['submit'])){
+    $select1 = $_POST['select1'];
+	echo $select1;
+	
+}
+?>
+
+#!/usr/bin/php
+#<?php
+#    $uri = $_SERVER['REQUEST_URI'];
+#$output = shell_exec('echo $uri > /tmp/test.txt');
+#echo "<pre>$output</pre>";
+#?>
